df4e34dca6d24941a87de69a8b5cbaf7921e495b
Hossain Ahmed <hossainahmed2002@gmail.com>
2025-11-01 22:15:41 +0000

    fixes t agent and added config

--- TabletopGames/src/group F/RHEA_Agent.java
+++ TabletopGames/src/group F/RHEA_Agent.java
@@ -1,17 +1,42 @@
+import org.jsoup.select.Evaluator;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
 public class RHEA_Agent {
+    private List<Individual> population;
+    private Individual bestPlanOfAction;
+    private HashMap<Integer, OpponentModel> opponentModels;
+    private RHEA_Config config;
+    private Evaluator evaluator;
+    private EvolutionEngine evolution;
+
+
+    public RHEA_Agent(RHEA_Config config, Evaluator evaluator, EvolutionEngine evolution){
+        this.config = config;
+        this.evaluator = evaluator;
+        this.evolution = evolution;
+        this.population = new ArrayList<>();
+        this.opponentModels = new HashMap<>();
+    }
+
+
     public void initaliseAgent(AbstractGameState state) {
-        ArrayList population = new ArrayList<>();
-        // bestPlanOfAction stores the best move for the agent to take
-        String bestPlanOfAction = null;
-        HashMap <> opponentModels = new HashMap<>();
-        for (int i=0; i<state.getNPlayers(); i++) {
-            if(i !=state.getCurrentPlayer()) {
-                opponentModels.put(new opponentModel());
+        this.population.clear();
+        this.bestPlanOfAction = null;
+        this.opponentModels.clear();
+
+        for (int i = 0; i < state.getNPlayers(); i++) {
+            if (i != state.getCurrentPlayer()) {
+                this.opponentModels.put(i, new OpponentModel(i));
             }
         }
-
     }
+
+    public int selectAction(AbstractGameState state) {
+        this.bestPlanOfAction = evolution.run(state);
+        return bestPlanOfAction.getActionSequence().get(0);
+    }
+
 }
