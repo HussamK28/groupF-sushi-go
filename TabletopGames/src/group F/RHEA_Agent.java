6816e755d17c37f70073066c11ae6aef52645ea0
Hussam Khan <HussamK2812@gmail.com>
2025-11-03 14:14:52 +0000

    Update RHEA_Agent.java

--- TabletopGames/src/group F/RHEA_Agent.java
+++ TabletopGames/src/group F/RHEA_Agent.java
@@ -1,29 +1,83 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Random;
+
 public class RHEA_Agent {
-    public void initaliseAgent(AbstractGameState state) {
-        ArrayList population = new ArrayList<>();
-        final int populationSize = 20;
-        final int horizonSize = 5;
-        final int maxGenerations = 20;
-        // bestPlanOfAction stores the best move for the agent to take
-        String bestPlanOfAction = null;
+    private List<Individual_Action> population;
+    private final int populationSize = 20;
+    private final int horizonSize = 5;
+    private final int maxGenerations = 20;
 
-        final Random random = new Random();
+    private Individual_Action bestIndividual;
+    private Random random;
+    private HashMap<Integer, OpponentModel> opponentModels;
 
 
-        HashMap <> opponentModels = new HashMap<>();
-        for (int i=0; i<state.getNPlayers(); i++) {
-            if(i !=state.getCurrentPlayer()) {
-                opponentModels.put(new opponentModel());
+    public void initialiseAgent(AbstractGameState state) {
+        random = new Random();
+        population = new ArrayList<>();
+        opponentModels = new HashMap<>();
+
+
+        for (int i = 0; i < state.getNPlayers(); i++) {
+            if (i != state.getCurrentPlayer()) {
+                opponentModels.put(i, new OpponentModel());
             }
         }
 
-        for (int i=0; i<populationSize; i++){
-
+        for (int i = 0; i < populationSize; i++) {
+            List<Integer> actions = randomActionSequence(horizonSize, state);
+            Individual_Action individual = new Individual_Action(actions);
+            population.add(individual);
         }
+    }
 
+    private List<Integer> randomActionSequence(int length, AbstractGameState state) {
+        List<Integer> sequence = new ArrayList<>();
+        int actionSpaceSize = state.getActionSpaceSize();
+
+        for (int i = 0; i < length; i++) {
+            sequence.add(random.nextInt(actionSpaceSize));
+        }
+        return sequence;
+    }
+
+    public void evolution(AbstractGameState state) {
+        for (int generation = 0; generation < maxGenerations; generation++) {
+            List<Individual_Action> newPop = new ArrayList<>();
+
+            while (newPop.size() < populationSize) {
+                Individual_Action parent = selectParent();
+                Individual_Action child = parent.clone();
+                child.mutate(state);
+                newPop.add(child);
+            }
+
+            population = newPop;
+
+            bestIndividual = Collections.max(population,
+                    Comparator.comparingDouble(Individual_Action::getFitness));
+        }
+    }
+
+    private Individual_Action selectParent() {
+        int t1 = random.nextInt(population.size());
+        int t2 = random.nextInt(population.size());
+
+        if (population.get(t1).getFitness() > population.get(t2).getFitness()) {
+            return population.get(t1);
+        } else {
+            return population.get(t2);
+        }
+    }
+
+    public int getNextAction() {
+        if (bestIndividual == null) {
+            throw new IllegalStateException("Agent not evolved yet!");
+        }
+        return bestIndividual.getActionSequence().get(0);
     }
 }
